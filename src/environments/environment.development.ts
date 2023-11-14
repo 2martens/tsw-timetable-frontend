3195c32db99cdcc768a1f293a0fd9f4dd15965d4
Jim Martens <github@2martens.de>
2023-11-14 23:08:30 +0100

    Adjust URL for API backend to be RESTful

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backendURL: "http://localhost:12000/timetable/v1",
+  backendURL: "http://localhost:12000/v1",
   keycloakURL: "https://id.2martens.de",
   realm: "2martens",
   clientId: "tsw-timetable-frontend"
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  backendURL: "https://api.2martens.de/timetable/v1",
+  backendURL: "https://api.2martens.de/v1",
   keycloakURL: "https://id.2martens.de",
   realm: "2martens",
   clientId: "tsw-timetable-frontend"
