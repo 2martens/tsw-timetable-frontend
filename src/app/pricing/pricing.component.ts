4a85f083fcc992a920e46c1cf80e830a9a63ad62
Jim Martens <github@2martens.de>
2023-11-19 11:00:48 +0100

    security(pricing): Replaced links to stripe to use test mode

--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -1,5 +1,6 @@
 import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {IonButtons, IonContent, IonHeader, IonMenuButton, IonTitle, IonToolbar} from "@ionic/angular/standalone";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-pricing',
@@ -7,28 +8,12 @@ import {IonButtons, IonContent, IonHeader, IonMenuButton, IonTitle, IonToolbar} 
   styleUrls: ['./pricing.component.scss'],
   standalone: true,
   imports: [
-    IonContent, IonHeader, IonToolbar, IonTitle, IonButtons, IonMenuButton
+    IonContent, IonHeader, IonToolbar, IonTitle, IonButtons, IonMenuButton, AsyncPipe,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class PricingComponent {
 
-  pricingTableId = "prctbl_1OBwBiB0O150WkrEBWLbMFfX";
-  publishableKey = "pk_live_2eOLaGr7ItE7d4xlB4HWrKp800JyKpBWcV";
-  uuid = Math.random().toString(16).slice(2);
-
-
   constructor() {
   }
-
-  get url(): string {
-    return 'https://js.stripe.com/v3/pricing-table-app.html?prctbl_id='
-      + this.pricingTableId
-      + '&pk='
-      + this.publishableKey
-      + '&uuid='
-      + this.uuid
-      + '&lo=undefined&customerEmail=undefined';
-  }
-
 }
