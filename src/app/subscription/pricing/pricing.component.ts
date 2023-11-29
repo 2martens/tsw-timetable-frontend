e23acc8ada486f03faea42d1d8310c608d7fe648
Jim Martens <github@2martens.de>
2023-11-29 19:37:18 +0100

    feat(subscription): Added feedback message on cancel

--- src/app/subscription/pricing/pricing.component.ts
+++ src/app/subscription/pricing/pricing.component.ts
@@ -1,4 +1,4 @@
-import {Component, CUSTOM_ELEMENTS_SCHEMA, inject} from '@angular/core';
+import {Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnDestroy} from '@angular/core';
 import {
   IonButton,
   IonButtons,
@@ -33,9 +33,13 @@ import {FormsModule, NgForm} from "@angular/forms";
 import {getProducts} from "../store";
 import {Store} from "@ngrx/store";
 import {subscribeAction} from "../store/subscription.actions";
-import {map, Observable} from "rxjs";
+import {filter, map, Observable, Subscription} from "rxjs";
 import {SubscriptionState} from "../store/subscription.reducer";
 import {AuthService} from "../../auth/service/auth.service";
+import {Message} from "../../messages/model/message";
+import {MessagesState} from "../../messages/store/messages.reducer";
+import {ActivatedRoute, EventType, NavigationEnd, Router} from "@angular/router";
+import {addMessageAction} from "../../messages/store/messages.actions";
 
 @Component({
   selector: 'app-pricing',
@@ -49,7 +53,7 @@ import {AuthService} from "../../auth/service/auth.service";
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class PricingComponent {
+export class PricingComponent implements OnDestroy {
   private readonly store: Store<SubscriptionState> = inject(Store<SubscriptionState>);
   private readonly authService: AuthService = inject(AuthService);
 
@@ -61,11 +65,32 @@ export class PricingComponent {
 
   mode: string | number = 'monthly';
 
-  constructor() {
+  private messages: Record<string, Message> = {
+    cancel: {
+      text: $localize`You have cancelled the subscription process. No payments will be deducted.`,
+      color: 'primary',
+      durationInMs: 5000,
+    }
+  }
+  private subscription: Subscription;
+
+  constructor(private readonly messagesStore: Store<MessagesState>,
+              private readonly activatedRoute: ActivatedRoute,
+              private readonly router: Router) {
     addIcons({
       checkmarkCircleOutline,
       checkmarkCircleSharp,
-    })
+    });
+
+    this.subscription = this.router.events.pipe(
+      filter(event => event.type == EventType.NavigationEnd),
+      map(event => event as NavigationEnd),
+      filter(event => event.url.startsWith('/pricing?state='))
+    ).subscribe(() => this.triggerFeedbackMessage());
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   onSegmentChange(event: SegmentCustomEvent) {
@@ -74,5 +99,12 @@ export class PricingComponent {
 
   onSubmit(form: NgForm) {
     this.store.dispatch(subscribeAction({lookupKey: form.value['lookup_key']}));
+  }
+
+  private triggerFeedbackMessage() {
+    const state = this.activatedRoute.snapshot.queryParamMap.get('state') || '';
+    if (state in this.messages) {
+      this.messagesStore.dispatch(addMessageAction({message: this.messages[state]}));
+    }
   }
 }
